14de278b605ca86d07e8d3cbd15cd3e5037911d1
yohannavrojo <yohavale7883@gmail.com>
2022-05-07 11:27:47 -0300

    peticiones http

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,49 @@
-import login from "./login.css"
-
-const submitHandler=()=>{
-
-}
+import axios from "axios";
 
 function Login() {
+	
+	const submitHandler=e=>{
+	e.preventDefault();
+	const email=e.target.email.value;
+	const password=e.target.password.value;
+const regexEmail= /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// console.log(regexEmail.test(email));
+
+if (email=== ''|| password===''){
+	console.log('los campos no pueden estar vacios')
+	return;
+}
+if (email!== ''&& !regexEmail.test(email)){
+	console.log('Debes escribir una direccion de correo electronico valida')
+  return;
+}
+if (email!== 'challenge@alkemy.org'|| password!== 'react'){
+	console.log('credenciales invalidas')
+  return;
+}
+console.log('ok estamos listos para enviar la informacion')
+axios
+.post('http://challenge-react.alkemy.org',{email,password})
+.then(res=> { 
+  console.log(res.data)
+})
+}
   return (
     <>
-    <div className="container">
-	<div className="screen">
-		<div className="screen__content">
-			<form onSubmit={submitHandler} className="login">
-				<div className="login__field">
-					<i className="login__icon fas fa-user"></i>
-					<input type="text" className="login__input" placeholder="User name / Email"/>
-				</div>
-				<div className="login__field">
-					<i className="login__icon fas fa-lock"></i>
-					<input type="password" className="login__input" placeholder="Password"/>
-				</div>
-				<button className="button login__submit">
-					<span className="button__text">Log In Now</span>
-					<i className="button__icon fas fa-chevron-right"></i>
-				</button>				
-			</form>
-			<div className="social-login">
-				<h3></h3>
-				<div className="social-icons">
-					<a href="#" className="social-login__icon fab fa-instagram"></a>
-					<a href="#" className="social-login__icon fab fa-facebook"></a>
-					<a href="#" className="social-login__icon fab fa-twitter"></a>
-				</div>
-			</div>
-		</div>
-		<div className="screen__background">
-			<span className="screen__background__shape screen__background__shape4"></span>
-			<span className="screen__background__shape screen__background__shape3"></span>		
-			<span className="screen__background__shape screen__background__shape2"></span>
-			<span className="screen__background__shape screen__background__shape1"></span>
-		</div>		
-	</div>
-</div>
+    <h2>Formulario de login</h2>
+<form onSubmit={submitHandler}>
+    <label>
+        <span>Correo electrónico:</span><br/>
+        <input type="text"name="email"/><br/>
+    </label>
+    <br/>
+    <label>
+        <span>Contraseña:</span><br/>
+        <input type="password"name="password"/><br/>
+    </label>
+    <br />
+    <button type="submit">Ingresar</button>
+</form>
     </>
   );
 }
